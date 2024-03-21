bf8a9b13d9def54db5632c46f1c9a69f6eecb2da
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2024-03-21 14:31:34 -0500

    feat(core): create handlers for get and post routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 import { initDb } from './db/db';
 
 const app = new Elysia()
@@ -6,8 +6,52 @@ const app = new Elysia()
 	.get('/users', ({ query, db }) => {
 		return db.query('SELECT * FROM users ').all();
 	})
-	.get('/users/:id', (): string => 'single users handler')
-	.post('/users/', (): string => 'create users handler')
+	.get(
+		'/users/:id',
+		({ params, db }) => {
+			const { id } = params;
+
+			return db.query('SELECT * FROM users WHERE user_id = $user_id').get({
+				$user_id: id,
+			});
+		},
+		{
+			params: t.Object({
+				id: t.Numeric(),
+			}),
+		}
+	)
+	.post(
+		'/users/',
+		({ body, db }) => {
+			console.info('Intering user to the table');
+
+			const { first_name, last_name, email, about } = body;
+
+			const insertUser = db.prepare(
+				'INSERT INTO users (first_name,last_name,email,about) VALUES ($first_name, $last_name,$email,$about) RETURNING *'
+			);
+
+			const insertedUser = insertUser.get({
+				$first_name: first_name,
+				$last_name: last_name,
+				$email: email,
+				$about: about || null,
+			});
+
+			console.info(`Inserted user ${insertedUser}`);
+
+			return insertUser;
+		},
+		{
+			body: t.Object({
+				first_name: t.String(),
+				last_name: t.String(),
+				email: t.String(),
+				about: t.Optional(t.String()),
+			}),
+		}
+	)
 	.listen(3000);
 
 console.log(
